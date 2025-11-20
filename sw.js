46c666532f218bf9d19e544001ee7b0286640276
alanps <alanps3699@gmail.com>
2025-11-20 07:35:32 +0530

    initial

---
+++ sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('push',function(e){
+    var options = {
+        body:'This notification is send from server',
+        icon:'images/example.js',
+        vibrate:[100,50,100],
+        data:{
+            dateOfArrival:Date.now(),
+            primaryKey:'2'
+        },
+        actions:[{
+            action:'explore',
+            title:'Explore this new world',
+            icon:'images/checkmark.png'
+
+        },{action:'close',title:'close',icon:'images/xmark.png'}]
+    };
+    e.waitUntil(self.registration.showNotification('Hello world',options));
+})
